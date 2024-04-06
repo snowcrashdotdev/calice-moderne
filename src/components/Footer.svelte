d29e57d43f26112749d94bd7b16793cae3aeb3e2
Snow Crash <github@snowcrash.fun>
2024-04-06 15:37:23 -0400

    Add simple header and footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="text-center text-gray-900 bg-gray-400 p-5">
+    <p>Join us on Discord. Star and contribute on <a class="underline" href="https://github.com/snowcrashdotdev/calice-moderne">GitHub</a></p>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,4 @@
+<header class="flex items-center justify-between bg-gray-400 p-5">
+    <a href="/">Calice Cup</a>
+    <a href="/signin">Sign in</a>
+</header>
