6556cff64ea8577216f66426ea50a9ae8f4f62eb
Snow Crash <github@snowcrash.fun>
2024-04-07 10:59:48 -0400

    Add action and progressive enhancement: create a new tournament in db

---
+++ src/routes/dashboard/tournaments/new/+page.server.ts
@@ -0,0 +1,19 @@
+import api from '$lib/api'
+import { redirect } from '@sveltejs/kit'
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData()
+		const serlializedData = {}
+
+		for (let [field, value] of data.entries()) {
+			serlializedData[field] = value
+		}
+
+		const res = await api.post('/tournaments', serlializedData)
+
+		if (! res.slug) return res
+		
+		throw redirect(300, `/dashboard/tournaments/${res.slug}/edit`)
+	}
+};
--- src/routes/dashboard/tournaments/new/+page.svelte
+++ src/routes/dashboard/tournaments/new/+page.svelte
@@ -1,11 +1,16 @@
-<form class="flex flex-col gap-2 mx-auto p-4">
+<script lang="ts">
+    import { enhance } from "$app/forms";
+    export let form;
+</script>
+
+<form method="POST" use:enhance class="flex flex-col gap-2 mx-auto w-2/3 max-w-xl p-4">
     <h1>New Tournament</h1>
     <label for="title">Title</label>
     <input type="text" id="title" name="title">
     <label for="start">Start time</label>
     <input type="datetime-local" id="start" name="start">
     <label for="end">End time</label>
-    <input type="datetime-local">
+    <input type="datetime-local" id="end" name="end">
     <label for="description">Description</label>
     <textarea id="description" name="description"></textarea>
     <button type="submit">Create Tournament</button>
