5c9ed55552d143e456e007a1cda7592b11d498b9
Snow Crash <github@snowcrash.fun>
2024-04-08 09:35:32 -0400

    Chore: update favicon and title tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
   import { Footer, Header } from "$components";
 </script>
 
+<svelte:head>
+    <title>Tournaments Calice</title> 
+</svelte:head>
+
 <Header />
 
 <main class="flex flex-col flex-grow">
