62b4f5941c002654e190855e0d4b3f9eba2e9527
Snow Crash <github@snowcrash.fun>
2024-11-11 11:54:02 -0500

    Update token endpoint to return encoded JWTs

--- api/calice/routers/security.py
+++ api/calice/routers/security.py
@@ -1,20 +1,36 @@
 from typing import Annotated
+from datetime import datetime, timedelta, timezone
 from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+from calice.settings import settings
+from calice.models.validation.security import ReadToken, Token, TokenData
 from calice.models.validation.user import UserCreate, UserNew, UserRead
+from calice.models.orm.user import User
 from calice.repositories.user import UserRepository
+
+APP_SECRET = settings.app_secret
+JWT_ALGORITHM = "HS256"
+JWT_EXPIRE_MINUTES = 15
 
 router = APIRouter()
 
-oauth2scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 ph = PasswordHasher()
 
-def hash_password(password: str):
+invalid_credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Incorrect username or password.",
+    headers={"WWW-Authenticate": "Bearer"}
+)
+
+async def hash_password(password: str):
     return ph.hash(password)
 
-def verify_password(hash: str, password: str):
+async def verify_password(hash: str, password: str):
     valid = ph.verify(hash, password)
 
     if ph.check_needs_rehash(hash):
@@ -22,9 +38,62 @@ def verify_password(hash: str, password: str):
 
     return valid
 
-@router.post("/token")
-async def token():
-    pass
+async def authenticate_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], user_repository: Annotated[UserRepository, Depends(UserRepository)]):
+    user = await user_repository.find_one(username=form_data.username)
+
+    if user is None: return user
+
+    try:
+        await verify_password(user.hashed_password, form_data.password)
+    except VerifyMismatchError:
+        raise invalid_credentials_exception
+
+    return user
+
+async def create_access_token(data: TokenData, expires_delta: timedelta):
+    to_encode = data.model_dump()
+    to_encode.update({"exp": datetime.now(timezone.utc) + expires_delta})
+    encoded_jwt = jwt.encode(to_encode, APP_SECRET, algorithm=JWT_ALGORITHM)
+
+    return encoded_jwt
+
+async def current_user(token: Annotated[str, Depends(oauth2_scheme)], user_repository: Annotated[UserRepository, Depends(UserRepository)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        decoded_jwt = jwt.decode(token, APP_SECRET, algorithms=[JWT_ALGORITHM])
+        username: str = decoded_jwt.get("sub")
+
+        if username is None: raise credentials_exception
+
+        token_data = ReadToken(username=username)
+    except jwt.exceptions.InvalidTokenError:
+        raise credentials_exception
+    
+    user = user_repository.find_one(username=token_data.username)
+
+    if user is None: raise credentials_exception
+
+    return user
+
+@router.post("/token", response_model=Token)
+async def request_access_token(user: Annotated[User | None, Depends(authenticate_user)]):
+    if user is None:
+        raise invalid_credentials_exception
+
+    access_token =  await create_access_token(
+        data=TokenData(sub=user.username),
+        expires_delta=timedelta(minutes=JWT_EXPIRE_MINUTES)
+    )
+
+    return Token(
+        access_token=access_token,
+        token_type="bearer"
+    )
 
 @router.post("/signup", response_model=UserRead)
 async def signup(user: UserCreate, user_repository: Annotated[UserRepository, Depends(UserRepository)]):
