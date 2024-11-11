0f8243a0d3ff616db8d7a00b7cb2d376c6baea50
Snow Crash <github@snowcrash.fun>
2024-11-11 11:52:39 -0500

    Add app secret setting

--- api/calice/settings.py
+++ api/calice/settings.py
@@ -8,5 +8,6 @@ class Settings(BaseSettings):
     )
 
     database_url: str = 'postgresql+asyncpg://calice:calice@db:5432/calice_dev'
+    app_secret: str = 'c33de679efc3d867589fb34f3874e12942379b35cc246350f18dbd81032b0d5f'
 
 settings = Settings()
