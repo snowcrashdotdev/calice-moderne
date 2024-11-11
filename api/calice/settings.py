09c4d07bd68006c9d38087ed42ddb109955752a0
Snow Crash <github@snowcrash.fun>
2024-11-11 16:18:47 -0500

    Add example env files

--- api/calice/settings.py
+++ api/calice/settings.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_prefix='calice_',
-        env_file='.env',
+        env_file='.env.local',
         extra='ignore'
     )
 
-    database_url: str = 'postgresql+asyncpg://calice:calice@db:5432/calice_dev'
-    app_secret: str = 'c33de679efc3d867589fb34f3874e12942379b35cc246350f18dbd81032b0d5f'
+    database_url: str
+    app_secret: str
 
 settings = Settings()
