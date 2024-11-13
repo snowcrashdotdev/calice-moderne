996dda8e1fd11b5d8493209607e0a2acf7969395
Snow Crash <github@snowcrash.fun>
2024-11-13 07:48:15 -0500

    Add create tournament server action

--- ui/app/app/actions/tournament.ts
+++ ui/app/app/actions/tournament.ts
@@ -1,13 +1,46 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { TournamentFormSchema } from "@/app/lib/validation"
+import { request } from "@/app/lib/sdk"
+import { redirect } from "next/navigation"
+
+type TournamentFormValues = {
+    title: string
+    description: string
+    startTime: string
+    endTime: string
+}
+
 type TournamentFormState = {
-    values?: {}
-    errors?: {}
+    values?: TournamentFormValues,
+    errors?: {
+        title?: string[]
+        description?: string[]
+        startTime?: string[]
+        endTime?: string[]
+    },
     message?: string
 } | undefined
 
 export async function create(_state: TournamentFormState, formData: FormData) {
-    const values = Object.fromEntries(formData.entries())
+    const values = Object.fromEntries(formData.entries()) as any as TournamentFormValues
 
-    return {
-        values
+    const validatedTournamentForm = await TournamentFormSchema.safeParseAsync(values)
+
+    if (validatedTournamentForm.success === false) {
+        return {
+            values,
+            errors: validatedTournamentForm.error.flatten().fieldErrors
+        }
+    } else {
+        const newTournament = await request({
+            path: "/tournaments/",
+            method: "post",
+            data: validatedTournamentForm.data,
+        })
+
+        revalidatePath("/")
+        redirect(`/${newTournament.slug}`)
     }
 }
