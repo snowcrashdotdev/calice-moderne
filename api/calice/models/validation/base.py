39dfe383af865e85c776e0dbbb8036ed8f75546a
Snow Crash <github@snowcrash.fun>
2024-11-17 07:26:15 -0500

    Add ReadResource validation model for base resource fields

--- api/calice/models/validation/base.py
+++ api/calice/models/validation/base.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from uuid import UUID
 from pydantic import AliasGenerator, BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 
@@ -8,3 +10,11 @@ class Base(BaseModel):
         from_attributes=True,
         alias_generator=AliasGenerator(alias=to_camel),
     )
+
+
+class ReadResource(Base):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime
+    title: str
+    slug: str
