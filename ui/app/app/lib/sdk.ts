c3b3b0c28086d3586a1e0d01ec2a7cec020fceb2
Snow Crash <github@snowcrash.fun>
2024-11-17 11:08:15 -0500

    Stop using refresh tokens until I can figured out why they cause a memory leak..

--- ui/app/app/lib/sdk.ts
+++ ui/app/app/lib/sdk.ts
@@ -2,7 +2,7 @@ import "server-only"
 
 import createClient, { type Middleware } from "openapi-fetch"
 import type { paths } from "@/app/lib/calice"
-import { getSession } from "@/app/lib/session"
+import { getAccessToken } from "@/app/lib/session"
 import { env } from "@/app/lib/env.mjs"
 
 export function multipartSerializer(body: object): FormData {
@@ -18,10 +18,10 @@ const request = createClient<paths>({ baseUrl: env.API_URL })
 
 const authMiddleware: Middleware = {
     onRequest: async ({ request }) => {
-        const session = await getSession()
+        const token = await getAccessToken()
 
-        if (session?.token) {
-            request.headers.set("Authorization", `Bearer ${session.token}`)
+        if (token) {
+            request.headers.set("Authorization", `Bearer ${token}`)
         }
 
         return request
