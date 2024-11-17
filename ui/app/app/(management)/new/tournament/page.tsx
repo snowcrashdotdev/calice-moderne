b40633e3b7d1fb7483bbfdbc7c21ed0284bd94fa
Snow Crash <github@snowcrash.fun>
2024-11-17 10:48:41 -0500

    Wrap tournament form in Main component

--- ui/app/app/(management)/new/tournament/page.tsx
+++ ui/app/app/(management)/new/tournament/page.tsx
@@ -1,9 +1,10 @@
 import { TournamentForm } from "@/app/components/tournament/tournament-form";
+import { Main } from "@/app/components/layout";
 
 export default async function NewTournament() {
     return (
-        <main className="flex-1 flex justify-center items-center">
+        <Main>
             <TournamentForm />
-        </main>
+        </Main>
     )
 }
