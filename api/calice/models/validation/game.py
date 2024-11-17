8d33b60aa5e04da18a32c7a15d213e8a1c221761
Snow Crash <github@snowcrash.fun>
2024-11-17 09:07:47 -0500

    Add post and patch methods to games

--- api/calice/models/validation/game.py
+++ api/calice/models/validation/game.py
@@ -1,4 +1,15 @@
-from calice.models.validation import ReadResource
+from typing import Annotated
+from pydantic import Field
+from calice.models.validation.base import CreateResource, ReadResource, UpdateResource
+
 
 class GameRead(ReadResource):
-    filename: str
+    filename: str = None
+
+
+class GameCreate(CreateResource):
+    filename: Annotated[str, Field(max_length=32)] = None
+
+
+class GameUpdate(UpdateResource):
+    filename: Annotated[str, Field(max_length=32)] = None
--- api/calice/routers/game.py
+++ api/calice/routers/game.py
@@ -1,12 +1,28 @@
+from typing import List
 from fastapi import APIRouter
+from calice.dependencies.security import can_manage_tournament
 from calice.repositories import game
-from calice.models.validation.game import GameRead
+from calice.models.validation.game import GameCreate, GameRead, GameUpdate
 
 router = APIRouter(prefix="/games")
 
 
-@router.get("/", response_model=GameRead)
+@router.get("/", response_model=List[GameRead])
 async def index_games(game_repository: game.repository):
     games = await game_repository.find()
 
     return games
+
+
+@router.post("/", response_model=GameRead, dependencies=[can_manage_tournament])
+async def create_game(game: GameCreate, game_repository: game.repository):
+    new_game = await game_repository.create(game)
+
+    return new_game
+
+
+@router.patch("/", response_model=GameRead, dependencies=[can_manage_tournament])
+async def update_game(game: GameUpdate, game_repository: game.repository):
+    updated_game = await game_repository.update(game)
+
+    return updated_game
