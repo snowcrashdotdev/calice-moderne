86aef74417477ae0cbc4ca5f22148a1d1ff43dfe
Snow Crash <github@snowcrash.fun>
2024-11-19 00:09:27 -0500

    Fix ruleset class definition, add foreign key field

--- api/calice/models/orm/ruleset.py
+++ api/calice/models/orm/ruleset.py
@@ -1,14 +1,18 @@
+import uuid
 from typing import TYPE_CHECKING
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from calice.models.orm.base import Base, UuidMixin, TimestampMixin
+from calice.models.orm import Base
+from calice.models.orm.base import UuidMixin, TimestampMixin
 
 if TYPE_CHECKING:
-    from calice.models.orm.game import Game
+    from .game import Game
 
 class Ruleset(Base, UuidMixin, TimestampMixin):
     __tablename__ = "ruleset"
 
-    game = Mapped["Game"] = relationship(back_populates="rulesets")
-    title = Mapped[str] = mapped_column(String(64))
-    description = Mapped[str]
+    title: Mapped[str] = mapped_column(String(64))
+    description: Mapped[str]
+
+    game: Mapped["Game"] = relationship("Game", back_populates="rulesets")
+    game_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("game.id"))
