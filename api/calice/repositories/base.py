62c151797b7fcf7a23e52745df1edda48b37e1d2
Snow Crash <github@snowcrash.fun>
2024-11-19 01:00:09 -0500

    Update base repository type hints

--- api/calice/repositories/base.py
+++ api/calice/repositories/base.py
@@ -1,20 +1,20 @@
-from typing import List, Type, TypeVar, Union
+from typing import List, TypeAlias, Union
 from sqlalchemy import select
 from sqlalchemy import UnaryExpression
 from calice.dependencies.database import DatabaseSession
 from calice.models.validation.base import Base as BaseDataModel
 from calice.models.orm import Base as BaseORMModel
 
-Model = TypeVar("Model", bound=BaseORMModel)
-ModelClass = Type[Model]
-DataModel = TypeVar("DataModel", bound=BaseDataModel)
+SQLAlchemyModelType: TypeAlias = type[BaseORMModel]
+SQLAlchemyModel: TypeAlias = BaseORMModel
+PydanticModel: TypeAlias = BaseDataModel
 
 
 class NotFoundException(Exception):
     pass
 
 
-def RepositoryFactory(model: ModelClass, order_by: UnaryExpression = None):
+def RepositoryFactory(model: SQLAlchemyModelType, order_by: UnaryExpression = None):
     class BaseRepository:
         LIMIT = 100
 
@@ -24,8 +24,8 @@ def RepositoryFactory(model: ModelClass, order_by: UnaryExpression = None):
             BaseRepository.order_by = order_by
 
         @classmethod
-        async def create(cls, data: DataModel) -> Model:
-            db_model: Model = model(**data.model_dump())
+        async def create(cls, data: PydanticModel) -> SQLAlchemyModel:
+            db_model: SQLAlchemyModel = model(**data.model_dump())
             cls.session.add(db_model)
             await cls.session.commit()
             await cls.session.refresh(db_model)
@@ -33,14 +33,14 @@ def RepositoryFactory(model: ModelClass, order_by: UnaryExpression = None):
             return db_model
 
         @classmethod
-        async def find(cls, offset=0) -> List[Model]:
+        async def find(cls, offset=0) -> List[SQLAlchemyModel]:
             q = select(model).order_by(cls.order_by).limit(cls.LIMIT).offset(offset)
             res = await cls.session.scalars(q)
 
             return res.all()
 
         @classmethod
-        async def find_one(self, *args, **kwargs) -> Union[Model, None]:
+        async def find_one(self, *args, **kwargs) -> Union[SQLAlchemyModel, None]:
             attr, value = next(iter(kwargs.items()), None)
 
             q = select(model).where(getattr(model, attr) == value)
@@ -49,7 +49,9 @@ def RepositoryFactory(model: ModelClass, order_by: UnaryExpression = None):
             return res.unique().scalar_one_or_none()
 
         @classmethod
-        async def update(cls, data: DataModel = None, instance: Model = None) -> Model:
+        async def update(
+            cls, data: PydanticModel = None, instance: SQLAlchemyModel = None
+        ) -> SQLAlchemyModel:
             db_model = instance if instance else await cls.find_one(id=data.id)
 
             if not db_model:
