08072aeb31aa82cb7e6a22d89d908230b6fbbdf1
Snow Crash <github@snowcrash.fun>
2024-11-20 17:09:23 -0500

    Allow setting className from props for form, dialog

--- ui/app/app/components/form/form.tsx
+++ ui/app/app/components/form/form.tsx
@@ -1,5 +1,5 @@
 import type { FormHTMLAttributes } from "react";
 
 export function Form(props: FormHTMLAttributes<HTMLFormElement>) {
-    return <form {...props} className="flex flex-col gap-y-7" />
+    return <form {...props} className={`flex flex-col gap-y-7 ${props.className}`.trim()} />
 }
--- ui/app/app/components/layout/dialog.tsx
+++ ui/app/app/components/layout/dialog.tsx
@@ -2,13 +2,13 @@
 
 import { type DialogHTMLAttributes, useRef } from "react";
 
-export function Dialog({ open, onClose, onCancel, children }: DialogHTMLAttributes<HTMLDialogElement>) {
+export function Dialog({ open, onClose, onCancel, children, className }: DialogHTMLAttributes<HTMLDialogElement>) {
     const dialogRef = useRef<HTMLDialogElement>(null)
 
     if (open) dialogRef.current?.showModal()
 
     return (
-        <dialog ref={dialogRef} onClose={onClose} onCancel={onCancel} className="p-4 rounded">
+        <dialog ref={dialogRef} onClose={onClose} onCancel={onCancel} className={`p-4 rounded ${className ?? ""}`.trim()}>
             <button type="button" onClick={() => dialogRef.current?.close()}>Close</button>
             {children}
         </dialog>
