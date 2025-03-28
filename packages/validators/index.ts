20556b661e51b6b907e1c8eb9cb93d6c766730f4
Snow Crash <github@snowcrash.fun>
2025-03-28 19:39:39 -0400

    Add start/end time to create tournament schema, re-export ZodError

--- packages/validators/index.ts
+++ packages/validators/index.ts
@@ -1,10 +1,11 @@
-import { z } from "zod"
+import { z, ZodError } from "zod"
 
 export const createTournamentSchema = z.object({
     title: z.string(),
     description: z.string().optional(),
-    startTime: z.string().datetime(),
-    endTime: z.string().datetime()
+    startTime: z.string().datetime({ local: true }),
+    endTime: z.string().datetime({ local: true })
 })
 
 export const updateTournamentSchema = createTournamentSchema.partial()
+export { ZodError }
